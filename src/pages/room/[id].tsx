087acdb4e8b12c5dd4869c7e183f660982b3e777
xingxiang <xingxiang@twotreesgroup.com>
2023-02-24 23:31:54 +0800

    scaffold rest of the components

--- src/pages/room/[id].tsx
+++ src/pages/room/[id].tsx
@@ -12,9 +12,9 @@ export default function Room() {
           Menu
         </div>
       </header>
-      <main className="flex flex-col items-center">
+      <main className="flex flex-1 flex-col items-center gap-y-4">
         <div>Room ID: {id}</div>
-        <div className="rounded-lg bg-gradient-to-b from-[#F1F5F94D] to-[#F1F5F933] py-4 px-8">
+        <section className="rounded-lg bg-gradient-to-b from-[#F1F5F94D] to-[#F1F5F933] py-4 px-8">
           <div className="grid grid-cols-11 place-items-center gap-y-4 text-lg backdrop-opacity-30 ">
             <div className="col-start-1 col-end-3 justify-self-end text-xs font-medium">
               PLAYER
@@ -31,8 +31,30 @@ export default function Room() {
             <div className="col-span-2">2</div>
             <div className="col-span-2">1</div>
           </div>
-        </div>
+        </section>
+
+        <section className="h-24 w-full overflow-scroll bg-slate-500 bg-gradient-to-b to-slate-800">
+          History
+          <div>Row</div>
+          <div>Row</div>
+          <div>Row</div>
+          <div>Row</div>
+          <div>Row</div>
+          <div>Row</div>
+          <div>Row</div>
+        </section>
+
+        <section className="flex flex-col items-center gap-y-2">
+          <h2 className="text-xl uppercase">ROUND X</h2>
+          <div>User input quadrant</div>
+        </section>
       </main>
+      <footer className="flex-grow-0 z-40 w-full">
+        <div className="container flex h-16 flex-row items-center justify-between">
+          <button>Undo</button>
+          <button>Next</button>
+        </div>
+      </footer>
     </div>
   );
 }
